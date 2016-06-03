89bb22679ce1b8e2949bccc813317ae82b4ccffa
Yoshifumi sato <sato_yos@nifty.com>
2016-06-03 15:19:41 +0900

    Add some properties to AppliedRecord

--- applied_record.rb
+++ applied_record.rb
@@ -1,4 +1,13 @@
 # 出欠登録データを1件ごとに監視するオブジェクト
 
 class AppliedRecord
+  attr_accessor :name, :parent_name, :class, :number_in_class, :presence
+  attr_accessor :correct_student
+
+  def initialize(init_hash={})
+    init_hash.each do |key, value|
+      raise "プロパティ#{key}はサポートしていません。" unless self.respond_to? "#{key}="
+      self.send("#{key}=", value)
+    end
+  end
 end
--- spec/applied_record_spec.rb
+++ spec/applied_record_spec.rb
@@ -2,9 +2,30 @@ require_relative '../applied_record'
 
 describe "AppledRecord" do
   describe '初期化' do
-    let(:record){AppliedRecord.new}
-    it 'should be a valid object' do
-      expect(record).not_to be nil
+    context '引数無し' do
+      let(:record){AppliedRecord.new}
+      it 'should be a valid object' do
+        expect(record).not_to be nil
+      end
+    end
+    context '初期化データを与えて場合' do
+      let(:record){AppliedRecord.new(
+          name: '○○○子', parent_name: '○○×男', class: '阪急', number_in_class: 5,
+          presence: true,
+      )}
+      it 'should be a valid object' do
+        expect(record).to be_an AppliedRecord
+      end
+      it '初期化時に与えたデータが、パラメータとして設定されている' do
+        expect(record.name).to eq '○○○子'
+        expect(record.parent_name).to eq '○○×男'
+        expect(record.class).to eq '阪急'
+        expect(record.number_in_class).to be 5
+        expect(record.presence).to be true
+      end
+      it '初期化時には、「正しい生徒」を表すプロパティの値は居設定' do
+        expect(record.correct_student).to be nil
+      end
     end
   end
 end
