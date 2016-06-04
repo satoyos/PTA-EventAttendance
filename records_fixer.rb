80c2ace2fd6ac54bc33c09ed6dd14ef092bcf15d
Yoshifumi sato <sato_yos@nifty.com>
2016-06-04 22:04:17 +0900

    Implement "Read records from CSV" function of RecordsFixer

--- records_fixer.rb
+++ records_fixer.rb
@@ -1,3 +1,57 @@
+# 出欠登録レコードの一つ一つについて、それぞれがどの生徒のものなのかを特定する
+
+require_relative 'handle_csv_from_excel'
+require_relative 'applied_record'
 
 class RecordsFixer
+  attr_reader :records
+
+  def initialize(record_csv_path: nil)
+    return unless record_csv_path
+    @records = records_from_csv(record_csv_path)
+  end
+
+  private
+
+  def records_from_csv(csv_path)
+    raise '出欠登録レコードCSVのパスを引数で指定してください。' unless csv_path
+    raise "指定されたパスのファイルが見つかりません。[#{csv_path}]" unless File.exist? csv_path
+    recs = []
+    HandleCsvFromExcel.new(csv_path).csv.each do |row|
+      recs << record_from_row(row)
+    end
+    recs
+  end
+
+  def record_from_row(row)
+    AppliedRecord.new(
+                     name: row['在校生氏名'],
+                     number_in_class: row['出席番号'].to_i,
+                     class_name: row['クラス'],
+                     presence: (row['出欠'] == '出席'),
+                     parent_name: row['保護者氏名'],
+                     attendee_number: extract_attendie_number(row),
+                     comment: row['コメント'],
+                     date: Date.parse(row['回答日時']),
+    )
+  end
+
+  def extract_attendie_number(row)
+    m = row['参加人数'].match(/(\S+)名/)
+    return 0 unless m
+    case m[1]
+      when '１' ; 1
+      when '２' ; 2
+      when '３' ; 3
+      when '４' ; 4
+      when '５' ; 5
+      when '６' ; 6
+      when '７' ; 7
+      when '８' ; 8
+      when '９' ; 9
+      else ; raise "文字列[#{m[1]}]から数字を抽出できません。"
+    end
+  end
+
+
 end
--- spec/records_fixer_spec.rb
+++ spec/records_fixer_spec.rb
@@ -2,9 +2,34 @@ require_relative '../records_fixer'
 
 describe 'RecordsFixer' do
   describe '初期化' do
-    let(:fixer){RecordsFixer.new}
-    it 'should be a valid object' do
+    context '引数無しで初期化した場合' do
+      let(:fixer){RecordsFixer.new}
+      it 'should be a valid object' do
         expect(fixer).not_to be nil
+      end
+    end
+    context '出欠登録レコードのファイルパスを与えて初期化する' do
+      let(:fixer){RecordsFixer.new(record_csv_path: 'spec/out-2016-06-02-test.csv')}
+      it 'should be a RecordFixter' do
+        expect(fixer).to be_a RecordsFixer
+      end
+      it '出欠レコードを与えられたファイルから読み込んでいる' do
+        fixer.records.tap do |recs|
+          expect(recs).to be_an Array
+          expect(recs.size).to be 3
+          recs.first.tap do |r|
+            expect(r).to be_an AppliedRecord
+            expect(r.name).to eq '山田洋'
+            expect(r.number_in_class).to be 16
+            expect(r.class_name).to eq '中日'
+            expect(r.presence).to be true
+            expect(r.parent_name).to eq '山田耕筰'
+            expect(r.attendee_number).to be 1
+            expect(r.comment).to eq 'Eコース: オーストリア'
+            expect(r.date).to eq Date.new(2016, 6, 2)
+          end
+        end
+      end
     end
   end
 end
