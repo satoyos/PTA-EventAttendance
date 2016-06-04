c43180e87cd612abc933f866154f3c4682140c2d
Yoshifumi sato <sato_yos@nifty.com>
2016-06-04 22:03:01 +0900

    Add 'date' property to AppliedRecord
     - 回答日時を管理する。

--- applied_record.rb
+++ applied_record.rb
@@ -1,12 +1,13 @@
 # 出欠登録データを1件ごとに監視するオブジェクト
 
 require_relative 'init_with_hash_module'
+require 'date'
 
 class AppliedRecord
   include InitWithHash
 
   attr_accessor :name, :parent_name, :class_name, :number_in_class, :presence
-  attr_accessor :attendee_number, :comment
+  attr_accessor :attendee_number, :comment, :date
   attr_accessor :correct_student
 
 end
