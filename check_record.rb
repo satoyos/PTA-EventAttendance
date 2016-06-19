9b5b2a676a877f0d659fdd68ae061619bb09086c
Yoshifumi Sato <sato_yos@nifty.com>
2016-06-19 23:12:27 +0900

    [WIP!!] Writing CSV out by Course

--- check_record.rb
+++ check_record.rb
@@ -79,20 +79,25 @@ def students_of_course(course_name, peer)
   peer.all_students.select{|st| st.course == course_name}
 end
 
-def course_data_of(peer, idx: nil)
-  courses_from_json.map{|course|
+def course_data_of(idx=nil)
+  raise '何番目のデータを出力するのかを引数で指定してください。' unless idx
+  Course.all_courses.map{|course|
     student_data_of_course(course, )
 
   }
 end
 
-def csv_str_for_course(peer, records)
-  courses = courses_from_json(course_member_files_list_json)
-  max_students_num = courses.map{|course| students_of_course(courses, peer).size}.max
+def csv_str_for_course(fixer)
+  students_by_course = fixer.students_to_come.group_by{|st| st.course}
+  max_students_num = students_by_course.values.map{|students| students.size}.max
   CSV.generate do |csv|
+    #%ToDo: ここをちゃんと書く！
+    csv <<  students_by_course.values.map{|students| students.size}
+=begin
     (0..max_students_num-1).each do |idx|
-      csv << course_data_of(peer, idx: idx)
+      csv << course_data_of(idx)
     end
+=end
 
   end
 end
@@ -103,25 +108,24 @@ def csv_out_by_peer(path, peer, records, encoding: 'utf-8')
   end
 end
 
-def csv_out_by_course(path, peer, records, encoding: 'utf-8')
+def csv_out_by_course(path, fixer, encoding: 'utf-8')
   File.open(path, 'w:'+encoding) do |outfile|
-    outfile.puts csv_str_for_course(peer, records)
+    outfile.puts csv_str_for_course(fixer)
   end
 end
 
 fixer = RecordsFixer.new(record_csv_path: RECORD_CSV_PATH).
     set_peer_from_files_in('高2', files_path_json: class_member_files_list_json ).
     load_confirmed_data_and_check(confirm_history_json).
-    guess_students.save_confirmed_history(confirm_history_json)
+    guess_students.list_up_students_to_come.
+  save_confirmed_history(confirm_history_json)
 
 Course.read_courses_from_json(course_member_files_list_json, peer: fixer.peer)
 
 csv_out_by_peer(File.join(output_csv_folder, 'クラス別出欠状況.csv'),
                 fixer.peer, fixer.records, encoding: 'windows-31j')
 
-=begin
 csv_out_by_course(File.join(output_csv_folder, 'コース別出欠状況.csv'),
-                  fixer.peer, fixer.records, encoding: 'windows-31j')
-=end
+                  fixer, encoding: 'windows-31j')
 
 puts "\n【正常終了】"
